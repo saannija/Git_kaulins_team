d7f537b1c01f3bcabfedcf7409d7255ee2707c08
Romazanova <vlada.rmv@gmail.com>
2023-03-07 09:01:39 +0200

    Realizeta iespeja 1x mest kaulinu

--- src/Cirks.java
+++ src/Cirks.java
@@ -1,8 +1,12 @@
+import java.util.Random;
 
 class Cirks {
 
 	public static void main(String[] args) {
-		
+		int skaitlis;
+		Random rand = new Random();
+		skaitlis = rand.nextInt(6)+1;
+		System.out.println("Uzkrita skaitlis: "+skaitlis);
 
 	}
 
