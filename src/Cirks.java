02e807c006f548a06ef51456327b2433844f3dee
Romazanova <vlada.rmv@gmail.com>
2023-03-07 09:19:37 +0200

    Refactoreta programma, ieviesta metode 32 slaids

--- src/Cirks.java
+++ src/Cirks.java
@@ -2,11 +2,16 @@ import java.util.Random;
 
 class Cirks {
 
-	public static void main(String[] args) {
+	static void mestKaulinu(){
 		int skaitlis;
 		Random rand = new Random();
 		skaitlis = rand.nextInt(6)+1;
 		System.out.println("Uzkrita skaitlis: "+skaitlis);
+	}
+	
+	public static void main(String[] args) {
+		mestKaulinu();
+		
 
 	}
 
