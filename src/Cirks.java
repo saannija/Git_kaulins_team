dbd1d2418bd3acf18b9465689a7fc8cf57c8bdab
Romazanova <vlada.rmv@gmail.com>
2023-03-08 09:39:20 +0200

    Ieviesta iespeja mest kaulinu vairakas reizes

--- src/Cirks.java
+++ src/Cirks.java
@@ -2,15 +2,17 @@ import java.util.Random;
 
 class Cirks {
 
-	static void mestKaulinu(){
+	static void mestKaulinu(int reizes){
 		int skaitlis;
 		Random rand = new Random();
-		skaitlis = rand.nextInt(6)+1;
-		System.out.println("Uzkrita skaitlis: "+skaitlis);
+		for(int i = 1; i <= reizes; i++){
+			skaitlis = rand.nextInt(6)+1;
+			System.out.println("Uzkrita skaitlis: "+skaitlis);
+		}
 	}
 	
 	public static void main(String[] args) {
-		mestKaulinu();
+		mestKaulinu(3);
 		
 
 	}
