b96000944a640e8d47622ddb1bdb0461d84af1a5
Romazanova <vlada.rmv@gmail.com>
2023-03-08 09:48:58 +0200

    Integreta iespeja no konsoles ievadit metienu skaitu

--- src/Cirks.java
+++ src/Cirks.java
@@ -1,12 +1,19 @@
 import java.util.Random;
+import java.util.Scanner;
 
 class Cirks {
 
 	public static void main(String[] args) {
-		int skaitlis;
+		int skaitlis, reizes;
 		Random rand = new Random();
-		skaitlis = rand.nextInt(6)+1;
-		System.out.println("Uzkrita skaitlis: "+skaitlis);
+		Scanner scan = new Scanner(System.in);
+		System.out.println("Cik reizes mest kaulinu?");
+		reizes = scan.nextInt();
+		scan.close();
+		for(int i = 0; i < reizes; i++){
+			skaitlis = rand.nextInt(6)+1;
+			System.out.println("Uzkrita skaitlis: "+skaitlis);
+		}
 
 	}
 
