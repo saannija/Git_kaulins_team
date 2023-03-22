e89ea98744e7e19866df4a3c95c0d3f1d377ca8c
Liebus <sannija@inbox.lv>
2023-03-22 08:48:58 +0200

    Veikti koda uzlabojumi

--- src/Cirks.java
+++ src/Cirks.java
@@ -2,33 +2,36 @@ import java.util.Random;
 import java.util.Scanner;
 
 class Cirks {
-	static void mestKaulinu(int reizes){
-		int skaitlis;
+	static int mestKaulinu(int reizes){
+		int skaitlis = 0;
 		Random rand = new Random();
 		for(int i = 1; i <= reizes; i++){
 			skaitlis = rand.nextInt(6)+1;
 			System.out.println("Uzkrita skaitlis: "+skaitlis);
 		}
+		return skaitlis;
 	}
 	public static void main(String[] args) {
-		int reizes = 0, izvele;
+		int reizes = 0, izvele, pedejais = 0;
 		Scanner scan = new Scanner(System.in);
 		do{
-			System.out.println("1-Mest kaulinu\n2-Apturet");
+			System.out.println("1-Mest kaulinu\n2-Apskatit pedejo\n3-Apturet");
 			izvele=scan.nextInt();
-			switch(izvele){
+		switch(izvele){
 			case 1:
-		do{
-		System.out.println("Cik reizes mest kaulinu?");
-		reizes = scan.nextInt();
-		}while(reizes < 1);
-		break;
+				do{
+					System.out.println("Cik reizes mest kaulinu?");
+					reizes = scan.nextInt();
+				}while(reizes < 1);
+				pedejais = mestKaulinu(reizes);
+			break;
 			case 2:
+				System.out.println("Pedejais uzmestais ir "+pedejais);
+			case 3:
 				System.out.println("Programma aptureta!");
-		break;
+			break;
 		}
-		}while(izvele!=2);
-		mestKaulinu(reizes);
+		}while(izvele!=3);
 		scan.close();
 	}
 }
